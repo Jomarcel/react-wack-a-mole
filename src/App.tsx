1fb9b175e6b08e1ff2bea7d729a28d9bd501b2c0
jomarcel <jomarcel_n@outlook.com>
2023-10-23 23:00:43 +1000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [score, setScore] = useState(0);
+  const [grid, setGrid] = useState<boolean[][]>(
+    new Array(3)
+      .fill(false)
+      .map(() => new Array(3).fill(false))
+  )
+
+  const wackMole = (rowIndex: number, columnIndex: number) => {
+    if (!grid[rowIndex][columnIndex]) return;
+    const newMoles = [...grid];
+    newMoles[rowIndex][columnIndex] = false;
+    setGrid(newMoles);
+    setScore((prevScore) => prevScore + 1)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const numRows = grid.length;
+      const numCols = grid[0].length;
+      const randomColumnIndex = Math.floor(Math.random() * numCols)
+      const randomRowIndex = Math.floor(Math.random() * numRows)
+
+      setGrid((prevGrid) => {
+        const newMoles = [...prevGrid];
+        newMoles[randomRowIndex][randomColumnIndex] = true;
+        return newMoles;
+      });
+
+      setTimeout(() => {
+        setGrid((prevGrid) => {
+          if (!prevGrid[randomRowIndex][randomColumnIndex]) return prevGrid;
+          const newMoles = [...prevGrid];
+          newMoles[randomRowIndex][randomColumnIndex] = false;
+          return newMoles;
+        });
+      }, 900)
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [grid])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>Score: {score}</div>
+      <div className='grid'>
+        {
+          grid.map((row, rowIdx) =>
+            <div key={rowIdx} className='row'>{row.map((_column, columnIdx) =>
+              <div key={columnIdx}>
+                <img
+                  onClick={() => wackMole(rowIdx, columnIdx)}
+                  alt=''
+                  src={`${process.env.PUBLIC_URL}${grid[rowIdx][columnIdx] ? '/images/mole.png' : '/images/hole.png'}`}>
+                </img>
+              </div>)
+            }
+            </div>)
+        }
+      </div>
     </div>
   );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,49 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        readonly NODE_ENV: 'development' | 'production' | 'test';
+        readonly PUBLIC_URL: string;
+    }
+}
+
+declare module '*.bmp' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.gif' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.jpg' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.jpeg' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.png' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.webp' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.svg' {
+    import { FC, SVGProps } from 'react';
+
+    export const ReactComponent: FC<SVGProps<
+        SVGSVGElement
+    > & { title?: string }>;
+
+    const src: string;
+    export default src;
+}
